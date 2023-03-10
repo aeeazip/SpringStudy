d342fd62c5598727d4c44d4f8e603d7f0761d31f
aeeazip <codnjs0809@naver.com>
2023-03-10 11:57:26 +0900

    5주차 : JPA + JUnit 게시글 관리 예제

--- 5주차/JPATest/src/main/java/org/example/controller/posts/PostsController.java
+++ 5주차/JPATest/src/main/java/org/example/controller/posts/PostsController.java
@@ -1,13 +1,11 @@
 package org.example.controller.posts;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.PostsResponseDto;
 import org.example.dto.PostsSaveRequestDto;
 import org.example.service.posts.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // @Autowired 보다 생성자를 사용한 DI 권장
 // @RequiredArgsConstructor : final이 붙은 필드를 인자값으로 생성자를 생성
@@ -18,7 +16,12 @@ public class PostsController {
     private final PostsService postsService;
 
     @PostMapping("/upload")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){  // 게시글 등록
         return postsService.save(requestDto);
+    }
+
+    @GetMapping("/{postId}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
